0dcd2a4f5b0c9cde3f215f01f93c69555d6c2190
GabrielElero <gabriel.elero@escola.pr.gov.br>
2026-08-21 11:50:57 -0300

    atividade 3 feita

--- atividades/atv3.js
+++ atividades/atv3.js
@@ -14,15 +14,55 @@ app.get('/avisos', (req, res) => {
 
 
 const usuarios = [
-    'ana',
-    'bianca',
-    'joão',
-    'gabriel',
-    'enzo'
-  ]
+  'ana',
+  'bianca',
+  'joão',
+  'gabriel',
+  'enzo'
+]
 
 app.get('/usuarios', (req, res) => {
   res.json(usuarios);
+});
+
+const tarefas = [
+  'estudar',
+  'prova',
+  'tarefa',
+  'aula'
+]
+
+app.get('/tarefas', (req, res) => {
+  res.json(tarefas)
+});
+
+const aulas = [
+  'matematica',
+  'portugues',
+  'artes',
+  'progamaçao back-end front-end'
+]
+
+app.get('/aulas', (req, res) => {
+  res.json(aulas)
+});
+
+const ativo = [
+  'servidor ativo'
+]
+
+app.get('/ativo', (req, res) => {
+  res.json(ativo)
+});
+
+const turmas = [
+  '9°B',
+  '1°DS',
+  '2°DS'
+]
+
+app.get('/turmas', (req, res) => {
+  res.json(turmas)
 });
 
 app.listen(3000, () => {
